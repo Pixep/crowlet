cbfcb6ab75d3498d4915c17bd38c848b2bf2dc88
Adrien Leravat <Pixep@users.noreply.github.com>
2019-04-10 20:52:22 -0700

    Separate crawl.go and http.go tests

--- test/crawl_test.go
+++ test/crawl_test.go
@@ -1,81 +1,11 @@
 package crawler
 
 import (
-	"sort"
-	"sync"
 	"testing"
 	"time"
 
 	"github.com/Pixep/crowlet/pkg/crawler"
 )
-
-var waitMutex = &sync.Mutex{}
-var fetchedUrls []string
-
-func TestRunConcurrentGet(t *testing.T) {
-	resultChan := make(chan *crawler.HTTPResponse)
-	quitChan := make(chan struct{})
-	maxConcurrency := 3
-	urls := []string{
-		"url1",
-		"url2",
-		"url3",
-		"url4",
-		"url5",
-	}
-
-	waitMutex.Lock()
-	go crawler.RunConcurrentGet(mockHTTPGet, urls, crawler.HTTPConfig{}, maxConcurrency, resultChan, quitChan)
-	time.Sleep(time.Second)
-
-	if len(fetchedUrls) != maxConcurrency {
-		t.Fatal("Incorrect channel length of", len(fetchedUrls))
-		t.Fail()
-	}
-
-	waitMutex.Unlock()
-
-	resultChanOpen := true
-	for resultChanOpen == true {
-		select {
-		case _, resultChanOpen = <-resultChan:
-		}
-	}
-
-	sort.Strings(fetchedUrls)
-	if !testEq(fetchedUrls, urls) {
-		t.Fatal("Expected to crawl ", urls, " but crawled ", fetchedUrls, " instead.")
-		t.Fail()
-	}
-}
-
-func mockHTTPGet(url string, config crawler.HTTPConfig) *crawler.HTTPResponse {
-	fetchedUrls = append(fetchedUrls, url)
-	waitMutex.Lock()
-	waitMutex.Unlock()
-
-	return &crawler.HTTPResponse{URL: url}
-}
-
-func testEq(a, b []string) bool {
-
-	// If one is nil, the other must also be nil.
-	if (a == nil) != (b == nil) {
-		return false
-	}
-
-	if len(a) != len(b) {
-		return false
-	}
-
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-
-	return true
-}
 
 func TestMergeCrawlStats(t *testing.T) {
 	statsA := crawler.CrawlStats{
